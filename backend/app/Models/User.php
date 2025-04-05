fc03a482b24fc56c6477d5350b75279e93733604
tanimoto <yumenikki2@gmail.com>
2025-04-05 11:48:17 +0000

    ユーザーモデルに著者情報と「いいね」した記事を取得するメソッドを追加

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,18 +2,33 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * ユーザーモデル
+ * 
+ * システムの認証ユーザー情報を管理するモデルクラスです。
+ * 認証機能、通知機能、APIトークン機能を実装しています。
+ * 
+ * @property int $id
+ * @property string $name ユーザー名
+ * @property string $email メールアドレス
+ * @property \DateTime|null $email_verified_at メール確認日時
+ * @property string $password パスワード（ハッシュ済み）
+ * @property string|null $remember_token ログイン状態維持トークン
+ * @property \DateTime $created_at 作成日時
+ * @property \DateTime $updated_at 更新日時
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * 複数代入可能な属性
      *
      * @var array<int, string>
      */
@@ -24,7 +39,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * 非表示にする属性
      *
      * @var array<int, string>
      */
@@ -34,7 +49,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be cast.
+     * 型キャストする属性
      *
      * @var array<string, string>
      */
@@ -42,4 +57,25 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * このユーザーに関連する著者情報を取得
+     *
+     * @return HasOne
+     */
+    public function author(): HasOne
+    {
+        return $this->hasOne(Author::class);
+    }
+
+    /**
+     * このユーザーが「いいね」した記事を取得
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function likedArticles()
+    {
+        return $this->belongsToMany(Article::class, 'article_likes')
+            ->withTimestamps();
+    }
 }
